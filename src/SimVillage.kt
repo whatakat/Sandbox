955f16e4674a5e70983d69895358ef109165c4c4
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-08-21 00:09:11 +0300

    anonymous functions

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,7 +1,8 @@
 fun main(array: Array<String>) {
-    println({
+    val greetingFunction:(String)->String = {playerName->
         val currentYear = 2020
-        "Welcome to SimVillage, Major!(copyright $currentYear)"
-    }())
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
+    println(greetingFunction("Guyar"))
 
 }
