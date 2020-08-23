94bc2c8e8f07f5aee188759d254124a57ac12f3b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-08-23 21:29:11 +0300

    function reference

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,5 +1,5 @@
 fun main(array: Array<String>) {
-    runSimulation("Guyal"){playerName,numBuildings ->
+    runSimulation("Guyal",::printConstructionCost){playerName,numBuildings ->
         val currentYear = 2020
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
@@ -7,7 +7,14 @@ fun main(array: Array<String>) {
 
 
 }
-inline fun runSimulation(playerName:String,greetingFunction:(String,Int)->String){
+inline fun runSimulation(playerName:String,
+                         costPrinter:(Int)->Unit,
+                         greetingFunction:(String,Int)->String){
     val numBuildings = (1..3).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFunction(playerName,numBuildings))
+}
+fun printConstructionCost(numBuildings:Int){
+    val cost  = 500
+    println("construction cost: ${cost * numBuildings}")
 }
