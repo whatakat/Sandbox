60184471a3a1dfcf5fcb78b3c3b3f9dee2909627
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-08-24 23:19:26 +0300

    function type as return type

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,20 +1,19 @@
 fun main(array: Array<String>) {
-    runSimulation("Guyal",::printConstructionCost){playerName,numBuildings ->
+    runSimulation()
+
+
+}
+fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
+fun configureGreetingFunction():(String)->String{
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return {playerName:String->
         val currentYear = 2020
-        println("Adding $numBuildings houses")
+        numBuildings +=1
+        println("Adding $numBuildings $structureType")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-
-
-}
-inline fun runSimulation(playerName:String,
-                         costPrinter:(Int)->Unit,
-                         greetingFunction:(String,Int)->String){
-    val numBuildings = (1..3).shuffled().last()
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName,numBuildings))
-}
-fun printConstructionCost(numBuildings:Int){
-    val cost  = 500
-    println("construction cost: ${cost * numBuildings}")
 }
