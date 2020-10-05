500e39a730f74e4b08b41b1a6870432dfc5a7231
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-05 21:06:10 +0300

    add class Coin, Fedora

--- src/Generics.kt
+++ src/Generics.kt
@@ -1,3 +1,9 @@
 class LootBox<T>(item:T){
     private var loot: T = item
 }
+class Fedora(val name:String, val value: Int)
+class Coin(val value: Int)
+fun main(args: Array<String>){
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora",15))
+    val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+}
