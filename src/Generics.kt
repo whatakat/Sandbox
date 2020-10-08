c68bc17f08aec72ce59b00322d02fd5e2975aadb
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-08 23:07:19 +0300

    open class Loot

--- src/Generics.kt
+++ src/Generics.kt
@@ -1,4 +1,4 @@
-class LootBox<T>(item:T){
+class LootBox<T:Loot>(item:T){
     var open = false
     private var loot: T = item
     fun fetch(): T?{
@@ -8,8 +8,9 @@ class LootBox<T>(item:T){
         return lootModFunction(loot).takeIf { open }
     }
 }
-class Fedora(val name:String, val value: Int)
-class Coin(val value: Int)
+open class Loot(val value: Int)
+class Fedora(val name:String, value: Int):Loot(value)
+class Coin(value: Int):Loot(value)
 fun main(args: Array<String>){
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora",15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
