be0cb95fa0fae907089d8711c222231904605ef4
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-08 23:15:30 +0300

    correct main

--- src/Generics.kt
+++ src/Generics.kt
@@ -12,11 +12,12 @@ open class Loot(val value: Int)
 class Fedora(val name:String, value: Int):Loot(value)
 class Coin(value: Int):Loot(value)
 fun main(args: Array<String>){
-    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora",15))
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora",15),
+    Fedora("a dazzling magenta fedora",25))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
     lootBoxOne.open = true
-    lootBoxOne.fetch()?.run { println("You retrieve $name from the box!")}
-    val coin  = lootBoxOne.fetch(){
+    lootBoxOne.fetch(1)?.run { println("You retrieve $name from the box!")}
+    val coin  = lootBoxOne.fetch(0){
         Coin(it.value*3)
     }
     coin?.let { println(it.value) }
