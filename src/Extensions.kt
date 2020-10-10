213306c802164a2b328c935a5f6688297c439960
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-10 23:34:17 +0300

    fun <T> easyPrint

--- src/Extensions.kt
+++ src/Extensions.kt
@@ -1,6 +1,9 @@
 fun String.addEnthusiasm(amount: Int = 1) = this+"!".repeat(amount)
-fun Any.easyPrint() = println(this)
+fun <T> T.easyPrint():T {
+    println(this)
+    return this
+}
 fun main(array: Array<String>){
-    ("Madrigal has left the building".addEnthusiasm()).easyPrint()
+    "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     43.easyPrint()
 }
