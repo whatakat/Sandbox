261924d04221e7822b606644321ff4d07094d240
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-10-10 22:43:14 +0300

    Any.easyPrint for String

--- src/Extensions.kt
+++ src/Extensions.kt
@@ -1,1 +1,5 @@
 fun String.addEnthusiasm(amount: Int = 1) = this+"!".repeat(amount)
+fun Any.easyPrint() = println(this)
+fun main(array: Array<String>){
+    ("Madrigal has left the building".addEnthusiasm()).easyPrint()
+}
